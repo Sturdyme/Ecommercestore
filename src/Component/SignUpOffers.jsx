31d9ffdfd8b0fa51a31f44c16e0918419c8b4933
Sturdyme <emmanuel08135672350@gmail.com>
2026-02-16 03:30:33 -0800

    signup offers

--- src/Component/SignUpOffers.jsx
+++ src/Component/SignUpOffers.jsx
@@ -6,7 +6,7 @@ const SignUpOffers = () => {
     <section>
       <div className='bg-[#ded5d0] h-32 '> 
       <div className='flex justify-center '>
-        <Link target='blank' to="/signup"> 
+        <Link to="/signup"> 
         <button className='px-4 py-3 border border-black mt-10 bg-gray-200 items-center hover:bg-purple-300 hover:theme-text-white transition ease-in-out
          duration-400 font-semibold text-sm
        theme-text-black rounded-full'> Sign up for deals and offers!</button>
