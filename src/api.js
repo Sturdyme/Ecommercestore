cc1c3368f8f1b2e7bd81921933f84a8018398607
Sturdyme <emmanuel08135672350@gmail.com>
2026-04-24 16:40:55 -0700

    cors

--- src/api.js
+++ src/api.js
@@ -6,10 +6,12 @@ const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 const api = axios.create({
     // 2. Use the variable and append the '/api' suffix
     baseURL: `${BASE_URL}/api`,
+    withCredentials: true,
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
     },
 });
+axios.defaults.withCredentials = true;
 
 export default api;
